9e6ff5519b57e4572eb9a7120a69b5f9e1312a2a
Shu Wang <shu.wang@autotrader.co.uk>
2022-12-19 12:10:43 +0000

    Add test and logic for single spare scenario

--- exercises/kotlin/src/main/kotlin/Bowling.kt
+++ exercises/kotlin/src/main/kotlin/Bowling.kt
@@ -1,5 +1,13 @@
 internal class Bowling {
-    fun addScores(rolls: ArrayList<Int>): Int {
-        return rolls.sum()
+    fun addRolls(rolls: ArrayList<Int>): Int {
+        var totalScore = 0
+        val frames = rolls.chunked(2)
+        for (i in frames.indices) {
+            if (frames[i].sum() == 10) {
+                totalScore += frames[i+1][0]
+            }
+            totalScore += frames[i].sum()
+        }
+        return totalScore
     }
 }
--- exercises/kotlin/src/test/kotlin/bowling/BowlingTest.kt
+++ exercises/kotlin/src/test/kotlin/bowling/BowlingTest.kt
@@ -10,7 +10,7 @@ internal class BowlingTest {
         val bowling = Bowling()
         val rolls = arrayListOf(0,0, 0,0, 0,0, 0,0, 0,0,
             0,0, 0,0, 0,0, 0,0, 0,0)
-        assertEquals(bowling.addScores(rolls), 0)
+        assertEquals(bowling.addRolls(rolls), 0)
     }
 
     @Test
@@ -18,14 +18,38 @@ internal class BowlingTest {
         val bowling = Bowling()
         val rolls = arrayListOf(1,1, 1,1, 1,1, 1,1, 1,1,
             1,1, 1,1, 1,1, 1,1, 1,1)
-        assertEquals(bowling.addScores(rolls), 20)
+        assertEquals(bowling.addRolls(rolls), 20)
     }
 
     @Test
     fun addAnyComboScoresTest(){
         val bowling = Bowling()
-        val rolls = arrayListOf(1,9, 1,0, 0,7, 0,0, 2,0,
+        val rolls = arrayListOf(1,8, 1,0, 0,7, 0,0, 2,0,
             0,0, 0,8, 0,0, 0,0, 0,1)
-        assertEquals(bowling.addScores(rolls), 29)
+        assertEquals(bowling.addRolls(rolls), 28)
+    }
+
+    @Test
+    fun addScoresWithSingleSpareTest(){
+        val bowling = Bowling()
+        val rolls = arrayListOf(5,5, 1,0, 0,0, 0,0, 0,0,
+            0,0, 0,0, 0,0, 0,0, 0,0)
+        assertEquals(bowling.addRolls(rolls), 12)
+    }
+
+    @Test
+    fun addScoresWithMultipleSpareTest(){
+        val bowling = Bowling()
+        val rolls = arrayListOf(5,5, 1,9, 0,0, 3,7, 2,0,
+            0,0, 0,0, 0,0, 0,0, 0,0)
+        assertEquals(bowling.addRolls(rolls), 35)
+    }
+
+    @Test
+    fun addScoresWithSingleStrikeTest(){
+        val bowling = Bowling()
+        val rolls = arrayListOf(10, 1,1, 0,0, 0,0, 0,0,
+            0,0, 0,0, 0,0, 0,0, 0,0)
+        assertEquals(bowling.addRolls(rolls), 14)
     }
 }
